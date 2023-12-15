9140cf8cfcf5c45c4cdfb3bcf9734b99a27f87f2
KOREAVC <KOREAVC@DESKTOP-8341A0F>
2023-12-15 18:42:43 +0900

    1215 t

---
+++ t1215_01/src/test/java/com/java/www/T121501ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T121501ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
