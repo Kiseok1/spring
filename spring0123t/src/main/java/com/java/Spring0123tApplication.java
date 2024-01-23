5f2c67fd6bffa2889d2e07b1dadb487eb49ca8c4
KOREAVC <KOREAVC@DESKTOP-8341A0F>
2024-01-23 11:46:14 +0900

    1111

---
+++ spring0123t/src/main/java/com/java/Spring0123tApplication.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring0123tApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring0123tApplication.class, args);
+	}
+
+}
